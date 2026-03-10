779f7eec0ad36624522c8a46d9eb15680eedca60
rhtjdals00-png <gonbimom@gmail.com>
2026-03-10 14:47:12 +0900

    20260310_모듈_패키지

---
+++ game/__init__.py
@@ -0,0 +1,10 @@
+# game.__init__.py
+from .graphic.render import render_test
+# from .sound.echo import echo_test
+
+VERSION = 3.5
+
+def print_version_info():
+    print(f'The version of this game is {VERSION}.')
+
+print("Initalizing game...")
